9f6afeaad2dc76bb8bb311648782aa9b2dfb151f
Yash Patel <ypatel@princeton.edu>
2018-04-09 14:13:41 -0400

    Added moveable boat

---
+++ Assets/MoveBoat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoat : MonoBehaviour {
+
+    public float translateSpeed = 1.0f;
+    public float rotateSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -translateSpeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, translateSpeed * Time.deltaTime));
+        }
+    }
+}
