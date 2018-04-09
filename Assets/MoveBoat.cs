e78b666b810267a594621886e0aee8fbdcd315f9
Yash Patel <ypatel5400@gmail.com>
2018-04-09 17:37:48 -0400

    Added natural effects to boat

--- Assets/MoveBoat.cs
+++ Assets/MoveBoat.cs
@@ -4,29 +4,65 @@ using UnityEngine;
 
 public class MoveBoat : MonoBehaviour {
 
-    private const float MAX_SPEED = 15.0f;
+    // vector added to "get out" of the vehicle somewhat naturally
+    private Vector3 GETTING_OUT = new Vector3(0.0f, 0.25f,0.0f);
+    private const float MAX_SPEED = 30.0f;
     public float dragConstant = .75f;
 
     public Camera boatCamera;
     public Camera firstPersonCamera;
+    public GameObject player;
 
     private bool canEnterBoat;
     private bool insideBoat;
 
     private float linearSpeed;
     private float rotationSpeed;
+    private float acceleration;
 
+    private bool ShowEnterGUI;
+    private bool ShowExitGUI;
+    
     // Use this for initialization
     void Start () {
         canEnterBoat = true;
         insideBoat = false;
 
+        ShowEnterGUI = false;
+        ShowExitGUI = false;
+
         linearSpeed = 0.0f;
         rotationSpeed = 0.0f;
+        acceleration = 1.0f;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void OnGUI()
+    {
+        var centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+
+        if (ShowEnterGUI)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50),
+                "Press E to Enter", centeredStyle);
+        }
+
+        else if (ShowExitGUI)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50),
+                "Press Q to Exit", centeredStyle);
+        }
+
+        else
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50),
+                "", centeredStyle);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         linearSpeed = Mathf.Min(Mathf.Abs(linearSpeed), MAX_SPEED) * Mathf.Sign(linearSpeed);
         linearSpeed -= dragConstant * linearSpeed * Time.deltaTime;
         transform.Translate(new Vector3(0, 0, linearSpeed) * Time.deltaTime);
@@ -35,28 +71,38 @@ public class MoveBoat : MonoBehaviour {
         transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
         rotationSpeed -= dragConstant * rotationSpeed * Time.deltaTime;
 
-        if (insideBoat) { 
+        if (insideBoat) {
+            ShowEnterGUI = false;
+            ShowExitGUI  = true;
+
             firstPersonCamera.enabled = false;
             boatCamera.enabled = true;
 
+            acceleration = 1.0f;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                acceleration = 2.0f;
+            }
+
             if (Input.GetKey(KeyCode.D))
             {
-                rotationSpeed += 1.0f;
+                rotationSpeed += acceleration;
             }
             if (Input.GetKey(KeyCode.A))
             {
-                rotationSpeed -= 1.0f;
+                rotationSpeed -= acceleration;
             }
             if (Input.GetKey(KeyCode.W))
             {
-                linearSpeed += 1.0f;
+                linearSpeed += acceleration;
             }
             if (Input.GetKey(KeyCode.S))
             {
-                linearSpeed -= 1.0f;
+                linearSpeed -= acceleration;
             }
             if (Input.GetKey(KeyCode.Q))
             {
+                player.transform.position = transform.position + GETTING_OUT;
                 insideBoat = false;
             }
         }
@@ -67,15 +113,20 @@ public class MoveBoat : MonoBehaviour {
             boatCamera.enabled = false;
         }
 
-        if (canEnterBoat && !insideBoat && Input.GetKey(KeyCode.E))
+        if (canEnterBoat && !insideBoat)
         {
-            insideBoat = true;
+            if (Input.GetKey(KeyCode.E))
+            {
+                insideBoat = true;
+            }
         }
 
     }
 
     void OnTriggerEnter(Collider other)
     {
+        ShowEnterGUI = true;
+        ShowExitGUI = false;
         if (other.CompareTag("Player"))
         {
             canEnterBoat = true;
@@ -86,6 +137,8 @@ public class MoveBoat : MonoBehaviour {
     {
         if (other.CompareTag("Player"))
         {
+            ShowEnterGUI = false;
+            ShowExitGUI = false;
             canEnterBoat = false;
         }
     }
