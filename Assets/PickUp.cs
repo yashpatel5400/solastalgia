414ebd31f4ea32d90437f4186b51614f40b37e14
Yash Patel <ypatel@princeton.edu>
2018-04-09 22:29:23 -0400

    Added room

---
+++ Assets/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    private bool renderPickup;
+
+	// Use this for initialization
+	void Start () {
+        renderPickup = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
