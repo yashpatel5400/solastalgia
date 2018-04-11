ee6c6f61f7de8a9403f239ab33ca3b6a46dcab8b
Yash Patel <ypatel5400@gmail.com>
2018-04-11 01:07:27 -0400

    Added rigid body physics

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour {
 
-    private bool renderPickup;
+    private bool canPickUp;
+    private GameObject npc;
 
-	// Use this for initialization
-	void Start () {
-        renderPickup = false;
-
+    // Use this for initialization
+    void Start () {
+        canPickUp = false;
+        npc = null;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (canPickUp && npc != null)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                npc.transform.position = transform.position;
+            }
+        }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("NPC"))
+        {
+            canPickUp = true;
+            npc = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            canPickUp = false;
+            npc = null;
+        }
+    }
 }
