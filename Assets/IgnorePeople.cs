0086e2dedfe2a6d0546426435cc5f13e79e41493
Yash Patel <ypatel5400@gmail.com>
2018-04-23 11:03:51 -0400

    Fixed some boats

---
+++ Assets/IgnorePeople.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnorePeople : MonoBehaviour {
+
+    void Start()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (true) // collision.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+    }
