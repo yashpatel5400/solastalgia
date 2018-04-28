4e47a5ef3b92c493201cc21cc806910aca7fde60
Yash Patel <ypatel@princeton.edu>
2018-04-28 12:54:57 -0400

    Added bridges/animation

---
+++ Assets/CameraChange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+    public int animationTime;
+
+    public Camera animationCamera;
+    public Camera fpsCamera;
+    public Camera minimapCamera;
+
+    public GameObject ignored;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(animator());
+    }
+
+    IEnumerator animator()
+    {
+        animationCamera.GetComponent<Camera>().enabled = true;
+        fpsCamera.GetComponent<Camera>().enabled = false;
+        minimapCamera.GetComponent<Camera>().enabled = false;
+
+        ignored.SetActive(false);
+
+        yield return new WaitForSeconds(animationTime);
+
+        animationCamera.GetComponent<Camera>().enabled = false;
+        fpsCamera.GetComponent<Camera>().enabled = true;
+        minimapCamera.GetComponent<Camera>().enabled = true;
+
+        ignored.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
