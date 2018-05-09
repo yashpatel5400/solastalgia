2c730040daa93e6f5c560febdd1fca5ae822c9af
Matthew Parker <maddparker@gmail.com>
2018-05-09 13:38:52 -0400

    Added ResetScript and Reset Object

---
+++ Assets/Scripts/ResetScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScript : MonoBehaviour {
+
+	public static ResetScript instance;
+
+	public bool showCursor = true;
+
+	// Use this for initialization
+	void Start () {
+		if(instance == null){
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Cursor.visible: " + Cursor.visible);
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			SceneManager.LoadScene(0);	
+
+			Cursor.visible = showCursor;
+
+			Debug.Log("Cursor.visible: " + Cursor.visible);
+		}
+	}
+}
